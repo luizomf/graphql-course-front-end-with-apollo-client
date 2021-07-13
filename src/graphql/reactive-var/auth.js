b2fc1fb88e5027594ed1269733b8b784cc58ad35
Luiz Otávio <luizomf@gmail.com>
2021-07-13 01:24:53 -0300

    create auth reactive var

---
+++ src/graphql/reactive-var/auth.js
@@ -0,0 +1,46 @@
+const { makeVar } = require('@apollo/client');
+
+const authVarId = '__auth_data__';
+
+const initialValue = {
+  userName: '',
+  userId: '',
+  isLoggedIn: false,
+};
+
+const authVar = makeVar({ ...initialValue });
+
+const setVar = (userName = '', userId = '', isLoggedIn = false) => {
+  const authData = { userName, userId, isLoggedIn };
+  localStorage.setItem(authVarId, JSON.stringify(authData));
+  authVar(authData);
+};
+
+const getVar = () => {
+  return authVar();
+};
+
+const resetVar = () => {
+  localStorage.removeItem(authVarId);
+  authVar({ ...initialValue });
+};
+
+const hydrate = () => {
+  const localDataStr = localStorage.getItem(authVarId);
+  const authVarData = getVar();
+
+  if (!localDataStr) {
+    if (authVarData.isLoggedIn) {
+      resetVar();
+    }
+    return;
+  }
+
+  if (JSON.stringify(authVarData) === localDataStr) {
+    return;
+  }
+
+  const localDataObj = JSON.parse(localDataStr);
+
+  setVar(localDataObj.userName, localDataObj.userId, localDataObj.isLoggedIn);
+};
